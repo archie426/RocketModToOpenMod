232a16f060364cc01bb0ca783653aa9b6c1f10e8
archie426 <archiestride42@gmail.com>
2020-07-23 11:22:03 +0100

    Woops

--- RocketToOpenMod/Program.cs
+++ RocketToOpenMod/Program.cs
@@ -10,16 +10,24 @@ namespace RocketToOpenMod
         {
             Console.WriteLine("RocketMod to OpenMod");
             Console.WriteLine("If you haven't already please place this file inside the Rocket folder");
-            if (!File.Exists("Rocket.Permissions.Xml"))
-                Console.WriteLine("Wrong folder!");
             
+            if (!File.Exists("Rocket.Permissions.Xml"))
+            {
+                Console.WriteLine("Wrong folder! Exiting");
+                await Task.Delay(3000);
+                return;
+            }
+
             Console.WriteLine("1. Permissions ");
             IJob currentJob = new PermissionsJob();
             await currentJob.Do();
             
             Console.WriteLine("2. Rocket Core Translations");
             Console.WriteLine("Coming soon :p");
-            
+
+            await Task.Delay(5000);
+
+
         }
         
         
